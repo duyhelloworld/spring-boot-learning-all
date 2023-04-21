2140ee80e6c6e36b5cf76e47795d3c103bbecc49
duyhelloword <codedaovoiduy@gmail.com>
2023-04-21 20:40:52 +0700

    2 seletions for map params

--- student-management-system/src/main/java/com/example/studentmanagementsystem/repo/StudentRepo.java
+++ student-management-system/src/main/java/com/example/studentmanagementsystem/repo/StudentRepo.java
@@ -18,7 +18,8 @@ public interface StudentRepo extends JpaRepository<Student, Long> {
         // value = "SELECT s FROM Student s WHERE s.email = :email"
         value = "SELECT s FROM Student s WHERE s.email = ?1"
         )
-    Optional<Student> findByEmail(@Param("email") String email);
+    // Optional<Student> findByEmail(@Param("email") String email);
+    Optional<Student> findByEmail(String email);
 
     @Query(value = "SELECT s FROM Student s WHERE s.name LIKE CONCAT('%', ?1, '%')")
     List<Student> findByName(String name);
