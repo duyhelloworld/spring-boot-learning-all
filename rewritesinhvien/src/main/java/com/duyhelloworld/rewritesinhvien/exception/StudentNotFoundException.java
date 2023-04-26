d4e26dedc0d46b882acf80fbb3b9448c52604004
duyhelloworld <codedaovoiduy@gmail.com>
2023-04-26 22:12:27 +0700

    add exceptions

---
+++ rewritesinhvien/src/main/java/com/duyhelloworld/rewritesinhvien/exception/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.duyhelloworld.rewritesinhvien.exception;
+
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String msg){
+        super(msg);
+    }
+}
---
+++ rewritesinhvien/src/main/java/com/duyhelloworld/rewritesinhvien/exception/TeacherNotFoundException.java
@@ -0,0 +1,7 @@
+package com.duyhelloworld.rewritesinhvien.exception;
+
+public class TeacherNotFoundException extends Exception {
+    public TeacherNotFoundException(String msg){
+        super(msg);
+    }
+}
